f9553b1e66281b98e3e631218163542e471dd86b
10000JI <sksk436@naver.com>
2023-01-06 16:10:28 +0900

    build

---
+++ src/com/iu/api1/Strings/ex3/Ex3Main.java
@@ -0,0 +1,23 @@
+package com.iu.api1.Strings.ex3;
+
+public class Ex3Main {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+//		WeatherData weatherData =new WeatherData();
+//		WeatherDTO[] datas = weatherData.init();
+//		WeatherView weatherView = new WeatherView();
+//		for(int i=0;i<datas.length;i++) {
+//			System.out.println(datas[i].getCity());
+//			System.out.println(datas[i].getStatus());
+//		}
+//		weatherView.view(datas);
+		
+		WeatherController wc = new WeatherController();
+		wc.start();
+		
+		System.out.println("finish");
+	}
+	
+
+}
