b758d5ef7a45052f2b9af577facfb6e861c7ffc6
10000JI <sksk436@naver.com>
2023-01-06 10:12:19 +0900

    build

--- src/com/iu/api1/Strings/StringEx2.java
+++ src/com/iu/api1/Strings/StringEx2.java
@@ -9,8 +9,15 @@ public class StringEx2 {
 		String[] names = {"jpg","jpeg","gif","png"};
 		System.out.println("파일명을 입력");
 		String fileName = sc.next();
+		
 		for(int i=0;i<names.length;i++) {
-			
+			int idx = fileName.indexOf(".");
+				if(names[i].equals(fileName.substring(idx+1))) {
+					System.out.println("정상입니다.");
+					break;
+				}else {
+					System.out.println("비정상입니다.");
+			}
 		}
 		
 	}
---
+++ src/com/iu/api1/Strings/StringStudy4.java
@@ -0,0 +1,20 @@
+package com.iu.api1.Strings;
+
+public class StringStudy4 {
+
+	public static void main(String[] args) {
+		String str = "StarWars";
+		
+		String result = str.substring(4);
+		System.out.println(result);
+		
+		result = str.substring(4,6);
+		System.out.println(result);
+		
+		str = "test@naver.com";
+//		int idx = str.indexOf("@");
+//		result = str.substring(idx+1);
+		result = str.substring(str.indexOf("@")+1);
+		System.out.println(result);
+	}
+}
