d3552bb4211e080a2c56f1ca8378e375f99904f1
10000JI <sksk436@naver.com>
2023-01-07 08:35:20 +0900

    build

--- src/com/iu/api1/Strings/ex3/Ex3Main.java
+++ src/com/iu/api1/Strings/ex3/Ex3Main.java
@@ -4,6 +4,10 @@ public class Ex3Main {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
+
+//		WeatherController wc =new WeatherController();
+//		wc.start();
+//		//wc라는 변수에 객체의 주소를 만들기 때문에 재사용이 가능하다
 		
 		new WeatherController().start();
 		//한번만 사용할때
